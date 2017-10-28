abed468f176683a9ff74c127b609cd2d8e1bc51a
MOLLET Tristan <tristan.mollet@soprabanking.com>
2017-10-28 19:36:00 +0200

    invoke function

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,23 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private var invocations: Int = 0) {
+    operator fun invoke(): Invokable {
+        invocations++
+        return this
+    }
+
+    fun getNumberOfInvocations() = invocations
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
