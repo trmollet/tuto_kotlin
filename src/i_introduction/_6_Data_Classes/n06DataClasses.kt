c3c720630f20c1f7cff864c64c59e455ae4eec6f
MOLLET Tristan <tristan.mollet@soprabanking.com>
2017-10-28 16:09:45 +0200

    class Person using data annotation

--- src/i_introduction/_6_Data_Classes/n06DataClasses.kt
+++ src/i_introduction/_6_Data_Classes/n06DataClasses.kt
@@ -20,26 +20,4 @@ fun task6(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
 }
 
-class Person(val name: String, val age: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Person
-
-        if (name != other.name) return false
-        if (age != other.age) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + age
-        return result
-    }
-
-    override fun toString(): String {
-        return "Person(name=$name, age=$age)"
-    }
-}
+data class Person(val name: String, val age: Int)
