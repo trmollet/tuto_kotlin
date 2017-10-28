e68d9f2f3fa2b38391729035a70204b73489df0b
MOLLET Tristan <tristan.mollet@soprabanking.com>
2017-10-28 19:25:45 +0200

    for loop

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -11,6 +11,23 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) :
+        ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+    override fun contains(value: MyDate): Boolean = value in start..endInclusive
+}
+
+class DateIterator(private val dateRange: DateRange) : Iterator<MyDate> {
+    private var current: MyDate = dateRange.start
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY, 1)
+        return result
+    }
+
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+}
 
 enum class TimeInterval {
     DAY,
@@ -18,4 +35,9 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) = addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
