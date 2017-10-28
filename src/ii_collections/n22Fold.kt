2f9f4d1a837a819102730a9e0bbb9b722c3d2526
MOLLET Tristan <tristan.mollet@soprabanking.com>
2017-10-28 18:23:22 +0200

    fold + intersect

--- src/ii_collections/n22Fold.kt
+++ src/ii_collections/n22Fold.kt
@@ -8,14 +8,13 @@ fun example9() {
 // The same as
 fun whatFoldDoes(): Int {
     var result = 1
-    listOf(1, 2, 3, 4).forEach { element -> result = element * result}
+    listOf(1, 2, 3, 4).forEach { element -> result = element * result }
     return result
 }
 
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
     // Return the set of products ordered by every customer
-    return customers.fold(allOrderedProducts, {
-        orderedByAll, customer ->
-        todoCollectionTask()
+    return customers.fold(allOrderedProducts, { orderedByAll, customer ->
+        orderedByAll.intersect(customer.orderedProducts)
     })
 }
